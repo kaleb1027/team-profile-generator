b6f20d3b3960d5bb1faffc42adcb2d76db12842a
Kaleb Sherrill <kaleb1027@gmail.com>
2022-04-11 23:26:15 -0400

    Made user inquirer prompts for manager

--- index.js
+++ index.js
@@ -4,4 +4,56 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 const generateHTML = require("./src/generateHTML");
+const team = [];
+
+const addManager = () => {
+    return inquirer
+        .prompt([
+            {
+                type:"input",
+                message: "What is the team manager's name?",
+                name: "name",
+            },
+            {
+                type:"input",
+                message: "What is the team manager's ID?",
+                name: "id",
+            },
+            {
+                type:"input",
+                message: "What is the team manager's email address",
+                name: "email",
+            },
+            {
+                type:"input",
+                message: "What is the team manager's office number?",
+                name: "office",
+            },
+        ])
+        .then((managerInput) => {
+            const manager = new Manager(
+                managerInput.name,
+                managerInput.id,
+                managerInput.email,
+                managerInput.office
+            );
+            team.push(manager);
+            teamSelect();
+        });
+};
+
+const teamSelect = () => {
+    return inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Are you adding more members to the team?",
+                name: "select",
+                choices: ["Add an engineer", "Add an intern", "No thank you"],
+            },
+        ])
+        .then((userChoice) => {
+            if (userChoice)
+        })
+}
 
